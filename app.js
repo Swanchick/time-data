6162fbb86ddfb43b5512f103f0b71eddc7a253b7
Swanchick <61909965+Swanchick@users.noreply.github.com>
2022-08-19 19:33:00 +0300

    Update

--- app.js
+++ app.js
@@ -1,35 +1,48 @@
 const express = require('express')
 const expressLayouts = require("express-ejs-layouts")
 const fileUpload = require('express-fileupload');
+const bodyParser = require("body-parser")
+const fs = require("fs")
 
 const app = express()
 const port = 5000
+const dir = "./src/"
+
 
 app.use(expressLayouts)
 app.set("view engine", "ejs")
 app.use(fileUpload());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
-    res.render("index", {testString: "a"})
+    res.render("index")
 })
 
-app.post("/upload", (req, res) => {
+app.post("/upload", (req, res) => {    
     if (!req.files){
         console.log("File not found!")
         
         return
     }
 
+    let key = req.body.key
+    let _dir = `${dir}${key}/`
+
+
+    if (!fs.existsSync(_dir)){
+        fs.mkdirSync(_dir)
+    }
+
     let file = req.files.image
 
-    file.mv(`./src/${file.name}`, (err) => {
+    file.mv(`${_dir}${file.name}`, (err) => {
         if (err){
             console.log(err)
-            return res.status(500).send(err);
+            return res.status(500).send(err)
         }
 
-        res.send('File uploaded!');
+        res.send('File uploaded!')
     })
 })
 
-app.listen(port, () => console.info(`App is listening at port: ${port}.`))
+app.listen(port, '0.0.0.0', () => console.info(`App is listening at port: ${port}.`))
